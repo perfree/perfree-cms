7ed33cdf8a581bee151b749620d8ff27c9eff47e
yinpengfei <perfree@126.com>
2024-03-01 15:12:01 +0800

    插件接口文档 [√]

--- perfree-core/src/main/java/com/perfree/plugin/handle/OpenApiDocHandle.java
+++ perfree-core/src/main/java/com/perfree/plugin/handle/OpenApiDocHandle.java
@@ -1,11 +1,19 @@
 package com.perfree.plugin.handle;
 
+import cn.hutool.core.util.ReflectUtil;
+import com.perfree.plugin.PluginApplicationContextHolder;
 import com.perfree.plugin.PluginInfo;
-import com.perfree.plugin.commons.ReflectionUtils;
 import io.swagger.v3.oas.models.OpenAPI;
+import org.apache.commons.lang3.StringUtils;
 import org.springdoc.api.AbstractOpenApiResource;
+import org.springdoc.core.configurer.SpringdocActuatorBeanFactoryConfigurer;
+import org.springdoc.core.configurer.SpringdocBeanFactoryConfigurer;
+import org.springdoc.core.models.GroupedOpenApi;
+import org.springdoc.core.properties.SpringDocConfigProperties;
 import org.springdoc.core.service.OpenAPIService;
+import org.springframework.beans.factory.config.ConfigurableListableBeanFactory;
 import org.springframework.context.ApplicationContext;
+import org.springframework.util.CollectionUtils;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.lang.annotation.Annotation;
@@ -23,6 +31,9 @@ public class OpenApiDocHandle implements BasePluginRegistryHandler{
 
     private OpenAPIService openApiService;
     private List<Class<?>> restControllers;
+
+    private SpringDocConfigProperties springDocConfigProperties;
+
     public OpenApiDocHandle(ApplicationContext applicationContext) {
         this.applicationContext = applicationContext;
     }
@@ -30,12 +41,25 @@ public class OpenApiDocHandle implements BasePluginRegistryHandler{
     public void initialize() throws Exception {
         openApiService = applicationContext.getBean(OpenAPIService.class);
         AbstractOpenApiResource openApiResource = applicationContext.getBean(AbstractOpenApiResource.class);
-        restControllers = (List<Class<?>>) ReflectionUtils.getField(null, openApiResource.getClass(),
-                "ADDITIONAL_REST_CONTROLLERS", List.class);
+        Field additionalRestControllers = ReflectUtil.getField(openApiResource.getClass(), "ADDITIONAL_REST_CONTROLLERS");
+        additionalRestControllers.setAccessible(true);
+        restControllers = (List<Class<?>>) additionalRestControllers.get(null);
+        springDocConfigProperties = applicationContext.getBean(SpringDocConfigProperties.class);
     }
 
     @Override
     public void registry(PluginInfo pluginInfo) throws Exception {
+//        SpringDocConfigProperties.GroupConfig groupConfig = new SpringDocConfigProperties.GroupConfig();
+//        List<String> packagesToScan = new ArrayList<>();
+//        packagesToScan.add("com.exam");
+//        groupConfig.setPackagesToScan(packagesToScan);
+//
+//        List<String> pathsToMatch = new ArrayList<>();
+//        pathsToMatch.add("/**");
+//        groupConfig.setPathsToMatch(pathsToMatch);
+//        groupConfig.setDisplayName("测试");
+//        groupConfig.setGroup("测试");
+//        springDocConfigProperties.addGroupConfig(groupConfig);
         List<Class<?>> pluginClassList = pluginInfo.getClassList().stream().filter(item -> !item.isInterface()).toList();
         if(!pluginClassList.isEmpty()) {
             for (Class<?> aClass : pluginClassList) {
@@ -45,11 +69,9 @@ public class OpenApiDocHandle implements BasePluginRegistryHandler{
                 }
             }
         }
-        //applicationContext.addRegistryInfo(CONTROLLER_API_CLASS, apiClass);
-        Method setCachedOpenApiMethod =
-                ReflectionUtils.findMethod(openApiService.getClass(), "setCachedOpenAPI", OpenAPI.class);
+        Method setCachedOpenApiMethod = ReflectUtil.getMethod(openApiService.getClass(), "setCachedOpenAPI", OpenAPI.class);
         if(setCachedOpenApiMethod != null){
-            setCachedOpenApiMethod.invoke(openApiService, null);
+            setCachedOpenApiMethod.invoke(openApiService);
         }
     }
 
@@ -64,11 +86,9 @@ public class OpenApiDocHandle implements BasePluginRegistryHandler{
                 }
             }
         }
-        //applicationContext.addRegistryInfo(CONTROLLER_API_CLASS, apiClass);
-        Method setCachedOpenApiMethod =
-                ReflectionUtils.findMethod(openApiService.getClass(), "setCachedOpenAPI", OpenAPI.class);
+        Method setCachedOpenApiMethod = ReflectUtil.getMethod(openApiService.getClass(), "setCachedOpenAPI", OpenAPI.class);
         if(setCachedOpenApiMethod != null){
-            setCachedOpenApiMethod.invoke(openApiService, null);
+            setCachedOpenApiMethod.invoke(openApiService);
         }
     }
 
